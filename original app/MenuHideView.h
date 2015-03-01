a5bbe0c9f08681a3d31a76ad32d095a2b1641fc3
Ia Kawasaki <ia_0212justin-716cherry@softbank.ne.jp>
2015-03-01 23:48:34 +0900

    MenuHideViewを追加

---
+++ original app/MenuHideView.h
@@ -0,0 +1,13 @@
+//
+//  MenuHideView.h
+//  original
+//
+//  Created by ia on 2015/03/01.
+//  Copyright (c) 2015年 Life is tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuHideView : UIView
+
+@end
---
+++ original app/MenuHideView.m
@@ -0,0 +1,21 @@
+//
+//  MenuHideView.m
+//  original
+//
+//  Created by ia on 2015/03/01.
+//  Copyright (c) 2015年 Life is tech. All rights reserved.
+//
+
+#import "MenuHideView.h"
+
+@implementation MenuHideView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
